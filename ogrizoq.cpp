5c5a57a720295e8f350c1136093cf9ff8540f551
my0brain0is0dead <118047676+my0brain0is0dead@users.noreply.github.com>
2023-09-06 16:44:51 +0300

    Add files via upload

---
+++ ogrizoq.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Sum {
+private:
+	int x, y, s;
+public:
+	Sum(int valueX, int valueY) {
+		x = valueX;
+		y = valueY;
+	}
+	void Print() {
+		cout << "X = " << x << endl;
+		cout << "Y = " << y << endl;
+	}
+	void z
+	void init() {
+
+	}
+};
+
+int main()
+{
+	Sum zxс(3, 4);
+	Sum qwe();
+	zxс.Print();
+}
